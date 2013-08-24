dbafe7e354b799cac8c2b3284fb8831da86ddf62
UnwrittenFun <unwrittenfun@gmail.com>
2013-08-24 16:36:30 +0100

    Add teleport on collision

--- src/unwrittenfun/minecraft/wallteleporters/blocks/BlockWallTeleporter.java
+++ src/unwrittenfun/minecraft/wallteleporters/blocks/BlockWallTeleporter.java
@@ -3,7 +3,10 @@ package unwrittenfun.minecraft.wallteleporters.blocks;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import unwrittenfun.minecraft.wallteleporters.client.gui.creativetabs.WTCreativeTabs;
 import unwrittenfun.minecraft.wallteleporters.info.BlockInfo;
@@ -31,5 +34,20 @@ public class BlockWallTeleporter extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World world) {
         return null;
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World world, int x, int y, int z) {
+        return AxisAlignedBB.getAABBPool().getAABB(0, 0, 0, 0, 0, 0);
+    }
+
+    @Override
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity entity) {
+        if (!world.isRemote) {
+            if (entity instanceof EntityPlayerMP) {
+                EntityPlayerMP player = (EntityPlayerMP) entity;
+                player.playerNetServerHandler.setPlayerLocation(0, 60, 0, player.rotationYaw, player.rotationPitch);
+            }
+        }
     }
 }
